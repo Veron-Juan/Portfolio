2d4e34a0bd6dcc583bc7c1a20036cff47c958e07
Juan Veron <juan_juve97@hotmail.com>
2022-08-17 03:43:57 -0300

    imagenes en proyecto listas

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,6 +4,7 @@ import exterlink from "../assets/external-link.svg"
 import randomDog from "../assets/randomdog.jpg"
 
 
+
 export default function Portfolio(){
 
 
@@ -20,13 +21,15 @@ export default function Portfolio(){
             <div onMouseEnter={changueBg} onMouseLeave={changueBg} className="portfolio">
             <div className="portfolio-links">
                     
-                    <img className="portfolio-img" src="https://www.elgrupoinformatico.com/static/Noticias/2022/01/tener-hbo-max-gratis-1200x675.jpg" />
-                    <h3>Title project</h3>
+                    <div className="portfolio-img1"></div>
+                    <h3>Movies API</h3>
                     <p>this project is a bla bkasdd blasd asdjas sjd asd asasdasds dasdask das </p>
                     <div className="portfolio-links_button">
                     <button className="button-project" ><img src={exterlink}/>Project </button>
                     <button className="button-project"> <img className="abc" src={github}/>Repository</button>
-                    </div>
+                    
+                    
+                </div>
                     
                 </div>
                 <div className="portfolio-links">
@@ -42,7 +45,7 @@ export default function Portfolio(){
                 </div>
                 <div className="portfolio-links">
                     
-                    <img className="portfolio-img" src={randomDog} />
+                    <div className="portfolio-img3"></div>
                     <h3>Random Dogs</h3>
                     <p>This is one of my first projects, in which I use a REST API, you get random pictures of dogs, you can save them in favorites, delete them and upload a picture of your dog to save it. </p>
                     <div className="portfolio-links_button">
@@ -54,6 +57,18 @@ export default function Portfolio(){
 
                 <div className="portfolio-links">
                     
+                    <div className="portfolio-img4"></div>
+                    <h3>Score Rabbits</h3>
+                    <p>this project is a bla bkasdd blasd asdjas sjd asd asasdasds dasdask das </p>
+                    <div className="portfolio-links_button">
+                    <button className="button-project" ><img src={exterlink}/>Project </button>
+                    <button className="button-project"> <img className="abc" src={github}/>Repository</button>
+                    </div>
+                    
+                </div>
+
+                {/* <div className="portfolio-links">
+                    
                     <img className="portfolio-img" src="https://androidayuda.com/wp-content/uploads/2021/03/dogo.jpg" />
                     <h3>Title project</h3>
                     <p>this project is a bla bkasdd blasd asdjas sjd asd asasdasds dasdask das </p>
@@ -74,19 +89,7 @@ export default function Portfolio(){
                     <button className="button-project"> <img className="abc" src={github}/>Repository</button>
                     </div>
                     
-                </div>
-
-                <div className="portfolio-links">
-                    
-                    <img className="portfolio-img" src="https://androidayuda.com/wp-content/uploads/2021/03/dogo.jpg" />
-                    <h3>Title project</h3>
-                    <p>this project is a bla bkasdd blasd asdjas sjd asd asasdasds dasdask das </p>
-                    <div className="portfolio-links_button">
-                    <button className="button-project" ><img src={exterlink}/>Project </button>
-                    <button className="button-project"> <img className="abc" src={github}/>Repository</button>
-                    </div>
-                    
-                </div>
+                </div> */}
                 
                 
             </div>
